e17514c5552146d891cb00e181396558e6c99587
feiaiyue <aiyuefei1218@gmail.com>
2023-12-27 21:48:12 +0800

    0-intro

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # 2023-cpl-coding
-2023 CPL Coding
+> 2023 CPL Coding
+- new to c programming
+- learn some skills about coding
+
+## Cmake
+- cmake是用来管理c项目，方便管理运行，之前是用gcc去跑，他会自动的帮你编译、运行，
+- CMake也会调用类似gcc去编译
+- 需要写一个CMakeLists告诉cmake一些相关的信息
+- `cmake_minimum_required(VERSION 3.25)`CMake有很多版本，这个是告诉你最低要用哪个版本
+- `project(2023-cpl-coding C)`项目的名字以及用的C语言来写的
+
+## 默认初始化一些东西
+- `//` single-line comment gcc翻译的时候会完全忽略掉，但是对程序员很重要
+- 
---
+++ hello.c
@@ -0,0 +1,31 @@
+// single-line comment
+// Created by 费爱跃 on 2023/12/27.
+//
+
+/*
+ * this is a multi-line comment
+ * this is a multi-line comment
+ */
+
+// directive 叫做指令 不是 语句
+// .h : header file
+// stdio: standard input/output
+#include <stdio.h>
+
+/*
+ * main function
+ * y <- f(x)
+ * int : integer (return)
+ */
+
+// do not approve any parameter
+// in other functions we create, void must be added
+int main(void) {
+    // "Hello World\n" : String
+    // printf: print + f (format)
+    printf("Hello World\n");
+
+    // return statement (return to operating system)
+    // 0: exit code (normal terminate)
+    return 0;
+}
